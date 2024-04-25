ea2b4ebc0ef098344c3d2ca494b46e934ece566c
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-04-25 12:03:25 -0700

    Added JavaScript to blog.js to get page to present a list of blog posts pulled from local storage. Also added comments to all files to ensure understandability.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,53 @@
+// to navigate back to the homepage
 function goBack () {
     window.location.href = './index.html';
 }
+
+const blogList = document.querySelector("#blog-list");
+let blogs = [];
+
+function renderBlogs() {
+    blogList.innerHTML = ''; // to clear the blog list to prepare for new entries
+    for (let i = 0; i < blogs.length; i++) {
+    let blog = blogs[i];
+    
+     // create an article element for each blog
+    const article = document.createElement("article");
+
+    // set up the title of the blog post
+    const h2 = document.createElement("h2");
+    h2.className = 'title';
+    h2.textContent = "Title: " + blog.title;
+
+     // create and set up the paragraph content
+    const p = document.createElement("p");
+    p.className = 'content'; 
+    p.textContent = blog.content;
+
+    // create and set up the footer to display the author
+    const footer = document.createElement("footer");
+    footer.className = 'author';
+    footer.textContent = "Posted by: " +  blog.username;
+    
+    // append all elements to the article element
+    article.appendChild(h2);
+    article.appendChild(p);
+    article.appendChild(footer);
+    // append the article to the blog list in the DOM
+    blogList.appendChild(article); 
+  }
+}
+
+// initialize the application
+function init() {
+  const storedBlogs = JSON.parse(localStorage.getItem("blog"));  // load blogs from local storage
+
+  if (storedBlogs !== null) {
+    blogs = storedBlogs;
+  }
+  // render the blogs on the page
+  renderBlogs();
+}
+
+// Start the application
+init();
--- assets/js/form.js
+++ assets/js/form.js
@@ -6,24 +6,25 @@ const submitButton = document.querySelector('#submit');
 const informationEl = document.querySelector('#information');
 let blogCollection = JSON.parse(localStorage.getItem("blog")) || [];
 
-//Event Listener
+// event listener
 submitButton.addEventListener("click", function (event) {
-  //Preventing Default Behavior
+  // preventing default behavior
   event.preventDefault();
 
 if (usernameEl.value === "" || titleEl.value === "" || contentEl.value === "") {
     alert("Please fill out your username, title and blog text")
 } else {
-    //Creating Vent Post Object
+    // create a new blog post object
     const blogPost = {
       username: usernameEl.value,
       title: titleEl.value,
       content: contentEl.value,
     };
  blogCollection.push(blogPost);
-    //Storing Data in Local Storage
+    // storing data in local storage
     localStorage.setItem("blog", JSON.stringify(blogCollection));
 
+      // redirect to blog page
     window.location.replace("blog.html");
   }
 });
